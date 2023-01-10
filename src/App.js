c538e66714be6f6b61a076cb13b2972b9179030b
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-01-10 17:14:58 -0300

    Fix error module action not found in app.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react'
-import { Article,Brand, Actions  } from './components'
+
+import { Footer, Blog,Header, Features, WhatIA, Posibilities } from './containers'
+import { Brand,Action, Navbar } from './components'
 const App = () => {
   return (
-    <div>
-        <h1>AI_COM_DEV</h1>
+    <div className='App'>
+       <div className='gradient'>
+        <Navbar />
+        <Header />
+       </div>
+       <Brand />
+        <WhatIA />
+        <Features />
+        <Posibilities />
+        <Action />
+        <Blog />
+        <Footer />
     </div>
   )
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,5 @@
 export { default as Article } from './article/article'
 export { default as Brand } from './brand/Brand'
-export { default as Actions } from './actions/Actions'
+export { default as Action } from './actions/action'
 export { default as Navbar } from './navbar/Navbar'
 export { default as Features } from './features/Features'
