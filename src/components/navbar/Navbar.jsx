fbac5f61382e8a6e8f66e15727f2f24c161c7d00
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-01-10 19:45:11 -0300

    Add logo and icons (react-icons) for menu

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React,{useState} from 'react'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+
 import './Navbar.scss'
+import logo from '../../assets/logotipo.png'
 const Navbar = () => {
+  const [menu, setMenu] = useState(false)
   return (
-    <div>Navbar</div>
+  <div className="AI__navbar">
+    <div className="AI__navbar-links">
+      <div className="AI__navbar-links_logo">
+        <img src={logo} />
+      </div>
+      <div className="AI__navbar-links_container">
+        <p><a href="#home">Home</a></p>
+        <p><a href="#aicom">About IA?</a></p>
+        <p><a href="#possibility">Open-D</a></p>
+        <p><a href="#features">Cases</a></p>
+        <p><a href="#blog">Library</a></p>
+      </div> 
+    <div className="AI__navbar-sign">
+      <p>Login</p>
+      <button type='button'>Sign Up</button>
+    </div>    
+    </div>
+    <div className="AI__navbar-menu">
+      {menu ? <RiCloseLine size={28} onClick={() => setMenu(false)} /> : <RiMenu3Line size={28} onClick={() => setMenu(true)} />        
+      }
+    </div>
+  </div>
   )
 }
 
