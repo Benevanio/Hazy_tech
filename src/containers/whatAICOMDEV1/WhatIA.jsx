03e5579afc947d32c02258ec17152633ec96f30e
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-01-11 12:35:23 -0300

    add css for Ia descriptions

--- src/containers/whatAICOMDEV1/WhatIA.jsx
+++ src/containers/whatAICOMDEV1/WhatIA.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
-import './WHATAI.scss'
+import './WHATAI.css'
+import Features from '../features/Features'
 const WhatIA = () => {
   return (
-    <div>WhatIA</div>
+    <div className='AI__whatAI  section__margin' id='WAi'>
+      <div className='AI__whatAI-features'>
+        <Features />
+      </div>
+      <div className='AI__whatAI-heading'>
+        <h1 className="gradient__text">
+          As possibilidades de crescer estão no nivel de sua imaginação.
+        </h1>
+        <p>
+          Veja como a inteligência artificial pode ajudar a sua empresa a crescer
+        </p>
+      </div>
+      <div className='AI__whatAI-container'>
+        <Features />
+      
+        <Features />
+        <Features />
+      </div>
+    </div>
   )
 }
 
