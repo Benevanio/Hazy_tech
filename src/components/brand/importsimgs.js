e0486e6da634fa45f171cf1d30acc8ca29b34ffe
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-01-11 10:33:47 -0300

    Add icons from flatICons

---
+++ src/components/brand/importsimgs.js
@@ -0,0 +1,14 @@
+import google from '../../assets/google.png';
+import Slack from '../../assets/slack.png';
+
+import Atlassian from '../../assets/atlassian.png';
+import DropBox from '../../assets/dropbox-logo.png';
+import Shopify from '../../assets/shopify.png';
+
+export {
+    google,
+    Slack,
+    Atlassian,
+    DropBox,
+    Shopify 
+}
